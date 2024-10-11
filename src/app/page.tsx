4fb875941048978847cd99984f4440fc1b719ad1
marssaljr <marssaljr@gmail.com>
2024-10-10 21:06:20 -0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,212 @@
+import {
+  Clipboard,
+  FunnelSimple,
+  PencilSimpleLine,
+  Pulse,
+  CaretDown,
+  CirclesFour,
+  CalendarDots,
+  Package,
+  SketchLogo,
+  Bell,
+  MagnifyingGlass,
+  QuestionMark,
+} from "@phosphor-icons/react/dist/ssr";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="text-zinc-900 flex flex-row h-screen w-screen bg-gradient-to-br from-[#FEFDFD] via-[#F7FBF5] to-[#FEFDFD]">
+      <div className="p-1 gap-4 w-48 h-full bg-[#3C715E] text-white flex flex-col items-center">
+        <div className="w-full h-full flex flex-col">
+          <div className="flex flex-row px-4 py-4 gap-2 justify-start w-full items-center">
+            <SketchLogo size={20} color="#FFE193" />
+            <h1 className="font-bold text-1xl text-white">obantor</h1>
+          </div>
+          <div className="border-b w-full flex flex-col gap-2 py-4 px-1">
+            <div className="p-2 rounded-r-md w-full flex flex-row justify-between hover:bg-[#FFE193] hover:text-[#274F46] items-center">
+              <div className="flex flex-row gap-2 items-center">
+                <Clipboard size={20} />{" "}
+                <p className="text-xs font-bold">Tasks</p>
+              </div>
+              <p className="text-xs font-bold">12</p>
+            </div>
+            <div className="p-2 rounded-r-md w-full flex flex-row justify-between hover:bg-[#FFE193] hover:text-[#274F46] items-center">
+              <div className="flex flex-row gap-2 items-center">
+                <Pulse size={20} />
+                <p className="text-xs font-bold">Activities</p>
+              </div>
+              <CaretDown size={10} />{" "}
+            </div>
+          </div>
+          <div className="w-full flex flex-col gap-2 px-1 py-4">
+            <div className="p-2 rounded-r-md w-full flex flex-row justify-between hover:bg-[#FFE193] hover:text-[#274F46] items-center">
+              <div className="flex flex-row gap-2 items-center">
+                <CirclesFour size={20} />
+                <p className="text-xs font-bold">Dashboard</p>
+              </div>
+            </div>
+            <div className="p-2 rounded-r-md w-full flex flex-row justify-between hover:bg-[#FFE193] hover:text-[#274F46] items-center">
+              <div className="flex flex-row gap-2 items-center">
+                <CalendarDots size={20} />
+                <p className="text-xs font-bold">Schedule</p>
+              </div>
+            </div>
+          </div>
+          <div className="w-full flex flex-col gap-2 px-1 py-4">
+            <div className="p-2 rounded-r-md w-full flex flex-row justify-between hover:bg-[#FFE193] hover:text-[#274F46] items-center">
+              <div className="flex flex-row gap-2 items-center">
+                <Package size={20} />
+                <p className="text-xs font-bold">Products</p>
+              </div>
+              <CaretDown size={10} />{" "}
+            </div>
+            <div className="p-2 rounded-r-md w-full flex flex-row justify-between hover:bg-[#FFE193] hover:text-[#274F46] items-center">
+              <div className="flex flex-row gap-2 items-center">
+                <Package size={20} />
+                <p className="text-xs font-bold">Report</p>
+              </div>
+              <CaretDown size={10} />{" "}
+            </div>
+            <div className="p-2 rounded-r-md w-full flex flex-row justify-between hover:bg-[#FFE193] hover:text-[#274F46] items-center">
+              <div className="flex flex-row gap-2 items-center">
+                <Package size={20} />
+                <p className="text-xs font-bold">Note</p>
+              </div>
+              <CaretDown size={10} />{" "}
+            </div>
+            <div className="p-2 rounded-r-md w-full flex flex-row justify-between hover:bg-[#FFE193] hover:text-[#274F46] items-center">
+              <div className="flex flex-row gap-2 items-center">
+                <Package size={20} />
+                <p className="text-xs font-bold">Team</p>
+              </div>
+              <CaretDown size={10} />{" "}
+            </div>
+            <div className="p-2 rounded-r-md w-full flex flex-row justify-between hover:bg-[#FFE193] hover:text-[#274F46] items-center">
+              <div className="flex flex-row gap-2 items-center">
+                <Package size={20} />
+                <p className="text-xs font-bold">Clients</p>
+              </div>
+              <CaretDown size={10} />{" "}
+            </div>
+          </div>
+          <div className="w-full flex flex-col gap-2 px-1 py-4">
+            <div className="p-2 rounded-r-md w-full flex flex-row justify-between hover:bg-[#FFE193] hover:text-[#274F46] items-center">
+              <div className="flex flex-row gap-2 items-center">
+                <CirclesFour size={20} />
+                <p className="text-xs font-bold">Settings</p>
+              </div>
+            </div>
+            <div className="p-2 rounded-r-md w-full flex flex-row justify-between hover:bg-[#FFE193] hover:text-[#274F46] items-center">
+              <div className="flex flex-row gap-2 items-center">
+                <CalendarDots size={20} />
+                <p className="text-xs font-bold">Support</p>
+              </div>
+            </div>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <div className="border-t w-full flex flex-row gap-2 pt-2 items-center">
           <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+            className="rounded-full p-2"
+            width={48}
+            height={48}
+            alt="person"
+            src="https://randomuser.me/api/portraits/lego/3.jpg"
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          <div className="flex flex-col">
+            <p className="font-bold text-[8px]">Andrew Simon</p>
+            <p className="font-thin text-[8px]">andrew@gmail.com</p>
+          </div>
+        </div>
+      </div>
+      <div className="w-full flex flex-col">
+        <div className="border-b py-4 px-4 flex flex-row gap-24 w-full justify-between">
+          <div className="flex flex-col px-2 w-80">
+            <p className="text-xs text-zinc-400">Welcome,</p>
+            <p className="text-xs text-zinc-900 font-bold">Andrew Simon</p>
+          </div>
+          <div className="w-full shadow-md flex flex-row gap-2 rounded-full border px-2 py-1 bg-white items-center">
+            <MagnifyingGlass size={26} className="text-zinc-400" />
+            <input
+              className="border-none w-full bg-none"
+              placeholder="Search here..."
+            />
+          </div>
+          <div className="flex flex-row px-2 items-center justify-end gap-2">
+            <button className="p-2 rounded-full border bg-white">
+              <Bell size={20} className="text-zinc-400" />
+            </button>
+            <button className="p-2 rounded-full border bg-white">
+              <QuestionMark size={20} className="text-zinc-400" />
+            </button>
+          </div>
+        </div>
+        <div className="py-4 px-8 gap-4 flex flex-col">
+          <div className="border rounded-md p-2 flex flex-row gap-2">
+            <div className="flex flex-col border-r gap-2 px-2 w-80">
+              <p className="font-bold">April</p>
+              <p className="text-xs">Today is Saturday, April 6th, 2024</p>
+            </div>
+            <div className="flex flex-row justify-between w-full px-8 items-center">
+              <p className="font-bold">Board -</p>
+              <p className="flex flex-row items-center gap-2 text-xs">
+                Daily Tasks <CaretDown size={10} />{" "}
+              </p>
+              <div className="flex flex-row gap-2">
+                <button className="text-xs flex flex-row items-center gap-2 bg-none font-bold border rounded-md px-2 py-1">
+                  <FunnelSimple
+                    size={28}
+                    className="bg-white p-1 rounded-full border text-zinc-400"
+                  />
+                  Filters
+                </button>
+                <button className="text-xs flex flex-row items-center gap-2 bg-none font-bold border rounded-md px-2 py-1">
+                  <PencilSimpleLine
+                    className="bg-white p-1 rounded-full border text-zinc-400"
+                    size={28}
+                  />
+                  Create task
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-row gap-4">
+            <div className="border rounded-md p-4 gap-2 flex flex-col items-center w-64">
+              <div className="flex flex-row gap-2 justify-center items-center">
+                <div className="rounded-full w-2 h-2 bg-red-400"></div>
+                <div className="text-xs font-bold">To Do List</div>
+              </div>
+              <div className="w-full">
+                <button className="w-full h-8 flex justify-center items-center border rounded-md p-4 bg-white">
+                  +
+                </button>
+              </div>
+              <div className="bg-white shadow-md rounded-md p-4 w-full flex flex-col gap-2">
+                <div className="flex flex-row justify-between">
+                  <div className="flex flex-row gap-2 text-white">
+                    <button className="py-1 px-4 text-xs bg-[#3D91F7] rounded-full">
+                      Data
+                    </button>
+                    <button className="py-1 px-4 text-xs bg-[#DF81F7] rounded-full">
+                      Analysis
+                    </button>
+                  </div>
+                </div>
+                <div className="flex flex-col gap-2">
+                  <p className="font-bold w-full text-sm">
+                    Data Presentation Of IMW
+                  </p>
+                  <p className="text-xs text-zinc-500">
+                    IMW* Official Site - Empower your teams. Take your project
+                    data management skills to the next level. Concept to...
+                  </p>
+                </div>
+                <div></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
